efef9c882bbabed2a01f643267ad34287d039df2
edw-perzd <legna246.dav@gmail.com>
2023-10-03 12:40:35 -0600

    02 Finaliza Carrito

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 const listaCursos = document.querySelector('#lista-cursos');
+const tabla = document.querySelector('#lista-carrito tbody');
 
 let carrito = [];
 
@@ -12,23 +13,61 @@ function getCurso(e){
         item.price = padre.querySelector('p span').innerText;
         item.image = padre.parentElement.querySelector('img').src;
         item.cantity = 1;
-        // Verificar si el objeto existe en carrito
+        addItem(item);
+        // Llenar la tabla
+        showTable();
+    }
+}
+
+function addItem(item){
+    // Verificar si el objeto existe en carrito
         if(carrito.some(itemCarrito => item.id === itemCarrito.id )){
-            carrito = carrito.map(itemCarrito => {
+            carrito.forEach(itemCarrito => {
                 if(itemCarrito.id === item.id){
                     itemCarrito.cantity++;
-                    return itemCarrito;
                 }
-                else{
-                    return itemCarrito;
-                }
-            })
+            });
         }
         else{
             carrito.push(item);
         }
-        console.log(carrito);
+}
+
+function showTable(){
+    // Limpiar tabla
+    tabla.innerHTML = '';
+    // Iterar carrito para insertar
+    carrito.forEach(item => {
+        tabla.appendChild(createRow(item));
+    });
+}
+
+function createRow(item){
+    const row = document.createElement('tr');
+    let rowHTML = ``;
+    rowHTML += `<td><img src="${ item.image }" width="100px"></td>`;
+    rowHTML += `<td>${ item.name }</td>`;
+    rowHTML += `<td>${ item.price }</td>`;
+    rowHTML += `<td>${ item.cantity }</td>`;
+    const button = document.createElement('button');
+    button.setAttribute('data-id', item.id);
+    button.classList.add('btn');
+    button.innerHTML = 'X';
+    const td = document.createElement('td');
+    td.appendChild(button);
+    row.innerHTML = rowHTML;
+    row.appendChild(td);
+    return row;
+}
+
+function btnDelItem(e){
+    if(e.target.classList.contains('btn')){
+        const id = e.target.getAttribute('data-id');
+        // Eliminar de Carrito
+        carrito = carrito.filter(itemCarrito => itemCarrito.id !== id);
+        showTable();
     }
 }
 
-listaCursos.addEventListener('click', getCurso)
+listaCursos.addEventListener('click', getCurso);
+tabla.addEventListener('click', btnDelItem);
